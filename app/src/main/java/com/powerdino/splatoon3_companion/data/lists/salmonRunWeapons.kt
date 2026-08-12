fa8519be4f1165b3add17c231e4dfcacde440793
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2026-08-12 10:48:55 +0200

    Optimized images and added crossfade in salmon run weapon icons.

--- app/src/main/java/com/powerdino/splatoon3_companion/data/lists/salmonRunWeapons.kt
+++ app/src/main/java/com/powerdino/splatoon3_companion/data/lists/salmonRunWeapons.kt
@@ -10,88 +10,88 @@ val SalmonRunWeapons = listOf<WeaponsSalmon>(
     ),
     WeaponsSalmon(
         id = listOf(80,10080,20080),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/55/S3_Weapon_Main_.96_Gal.png/256px-S3_Weapon_Main_.96_Gal.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/55/S3_Weapon_Main_.96_Gal.png/64px-S3_Weapon_Main_.96_Gal.png"
     ),
     WeaponsSalmon(
         id= listOf(30,10030,20030),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c5/S3_Weapon_Main_Aerospray_MG.png/256px-S3_Weapon_Main_Aerospray_MG.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c5/S3_Weapon_Main_Aerospray_MG.png/64px-S3_Weapon_Main_Aerospray_MG.png"
     ),
     WeaponsSalmon(
         id = listOf(90,10090,20090),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/69/S3_Weapon_Main_Jet_Squelcher.png/256px-S3_Weapon_Main_Jet_Squelcher.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/69/S3_Weapon_Main_Jet_Squelcher.png/64px-S3_Weapon_Main_Jet_Squelcher.png"
     ),
     WeaponsSalmon(
         id = listOf(60,10060,20060),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/f/f6/S3_Weapon_Main_N-ZAP_%2785.png/256px-S3_Weapon_Main_N-ZAP_%2785.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/f/f6/S3_Weapon_Main_N-ZAP_%2785.png/64px-S3_Weapon_Main_N-ZAP_%2785.png"
     ),
     WeaponsSalmon(
         id=listOf(20,10020,20020),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/7/7b/S3_Weapon_Main_Splash-o-matic.png/256px-S3_Weapon_Main_Splash-o-matic.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/7/7b/S3_Weapon_Main_Splash-o-matic.png/64px-S3_Weapon_Main_Splash-o-matic.png"
     ),
     WeaponsSalmon(
         id = listOf(40,10040,20040),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/b/bf/S3_Weapon_Main_Splattershot.png/256px-S3_Weapon_Main_Splattershot.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/b/bf/S3_Weapon_Main_Splattershot.png/64px-S3_Weapon_Main_Splattershot.png"
     ),
     WeaponsSalmon(
         id = listOf(10,10010,20010),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/1e/S3_Weapon_Main_Splattershot_Jr..png/256px-S3_Weapon_Main_Splattershot_Jr..png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/1e/S3_Weapon_Main_Splattershot_Jr..png/64px-S3_Weapon_Main_Splattershot_Jr..png"
     ),
     WeaponsSalmon(
         id = listOf(100,20100),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/10/S3_Weapon_Main_Splattershot_Nova.png/256px-S3_Weapon_Main_Splattershot_Nova.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/10/S3_Weapon_Main_Splattershot_Nova.png/64px-S3_Weapon_Main_Splattershot_Nova.png"
     ),
     WeaponsSalmon(
         id = listOf(70,10070,20070),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/7/77/S3_Weapon_Main_Splattershot_Pro.png/256px-S3_Weapon_Main_Splattershot_Pro.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/7/77/S3_Weapon_Main_Splattershot_Pro.png/64px-S3_Weapon_Main_Splattershot_Pro.png"
     ),
     WeaponsSalmon(
         id = listOf(0,10000,20000),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/59/S3_Weapon_Main_Sploosh-o-matic.png/256px-S3_Weapon_Main_Sploosh-o-matic.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/59/S3_Weapon_Main_Sploosh-o-matic.png/64px-S3_Weapon_Main_Sploosh-o-matic.png"
     ),
     WeaponsSalmon(
         id = listOf(310,10310,20310),
-        image ="https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/5a/S3_Weapon_Main_H-3_Nozzlenose.png/256px-S3_Weapon_Main_H-3_Nozzlenose.png"
+        image ="https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/5a/S3_Weapon_Main_H-3_Nozzlenose.png/64px-S3_Weapon_Main_H-3_Nozzlenose.png"
     ),
     WeaponsSalmon(
         id = listOf(300, 10300,20300),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/2/2c/S3_Weapon_Main_L-3_Nozzlenose.png/256px-S3_Weapon_Main_L-3_Nozzlenose.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/2/2c/S3_Weapon_Main_L-3_Nozzlenose.png/64px-S3_Weapon_Main_L-3_Nozzlenose.png"
     ),
     WeaponsSalmon(
         id = listOf(400,10400,20400),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/cb/S3_Weapon_Main_Squeezer.png/256px-S3_Weapon_Main_Squeezer.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/cb/S3_Weapon_Main_Squeezer.png/64px-S3_Weapon_Main_Squeezer.png"
     ),
 
 
     // Rollers
     WeaponsSalmon(
         id = listOf(1010,11010,21010),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c7/S3_Weapon_Main_Splat_Roller.png/384px-S3_Weapon_Main_Splat_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c7/S3_Weapon_Main_Splat_Roller.png/64px-S3_Weapon_Main_Splat_Roller.png"
     ),
     WeaponsSalmon(
         id = listOf(1040,21040),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8e/S3_Weapon_Main_Big_Swig_Roller.png/256px-S3_Weapon_Main_Big_Swig_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8e/S3_Weapon_Main_Big_Swig_Roller.png/64px-S3_Weapon_Main_Big_Swig_Roller.png"
     ),
     WeaponsSalmon(
         id = listOf(1000,11000,21000),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/2/21/S3_Weapon_Main_Carbon_Roller.png/256px-S3_Weapon_Main_Carbon_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/2/21/S3_Weapon_Main_Carbon_Roller.png/64px-S3_Weapon_Main_Carbon_Roller.png"
     ),
     WeaponsSalmon(
         id = listOf(1020,11020,21020),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/5e/S3_Weapon_Main_Dynamo_Roller.png/256px-S3_Weapon_Main_Dynamo_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/5/5e/S3_Weapon_Main_Dynamo_Roller.png/64px-S3_Weapon_Main_Dynamo_Roller.png"
     ),
     WeaponsSalmon(
         id = listOf(1030, 11030,21030),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/d/d2/S3_Weapon_Main_Flingza_Roller.png/256px-S3_Weapon_Main_Flingza_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/d/d2/S3_Weapon_Main_Flingza_Roller.png/64px-S3_Weapon_Main_Flingza_Roller.png"
     ),
     WeaponsSalmon(
         id = listOf(1040, 21040),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8e/S3_Weapon_Main_Big_Swig_Roller.png/256px-S3_Weapon_Main_Big_Swig_Roller.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8e/S3_Weapon_Main_Big_Swig_Roller.png/64px-S3_Weapon_Main_Big_Swig_Roller.png"
     ),
 
     //Chargers
     WeaponsSalmon(
         id = listOf(2000,12000,22000),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/3/3c/S3_Weapon_Main_Classic_Squiffer.png/256px-S3_Weapon_Main_Classic_Squiffer.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/3/3c/S3_Weapon_Main_Classic_Squiffer.png/64px-S3_Weapon_Main_Classic_Squiffer.png"
     ),
     WeaponsSalmon(
         id = listOf(2010,12010,22010),
@@ -103,7 +103,7 @@ val SalmonRunWeapons = listOf<WeaponsSalmon>(
     ),
     WeaponsSalmon(
         id = listOf(2030,12030,22030),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/10/S3_Weapon_Main_E-liter_4K.png/256px-S3_Weapon_Main_E-liter_4K.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/1/10/S3_Weapon_Main_E-liter_4K.png/64px-S3_Weapon_Main_E-liter_4K.png"
     ),
     WeaponsSalmon(
         id = listOf(2040,12040,22040),
@@ -111,11 +111,11 @@ val SalmonRunWeapons = listOf<WeaponsSalmon>(
     ),
     WeaponsSalmon(
         id= listOf(2050, 12050,22050),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/6d/S3_Weapon_Main_Bamboozler_14_Mk_I.png/256px-S3_Weapon_Main_Bamboozler_14_Mk_I.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/6d/S3_Weapon_Main_Bamboozler_14_Mk_I.png/64px-S3_Weapon_Main_Bamboozler_14_Mk_I.png"
     ),
     WeaponsSalmon(
         id= listOf(2060,12060,22060),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8a/S3_Weapon_Main_Goo_Tuber.png/256px-S3_Weapon_Main_Goo_Tuber.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/8/8a/S3_Weapon_Main_Goo_Tuber.png/64px-S3_Weapon_Main_Goo_Tuber.png"
     ),
     WeaponsSalmon(
         id = listOf(2070,22070),
@@ -295,11 +295,11 @@ val SalmonRunWeapons = listOf<WeaponsSalmon>(
     // Grizzco Weapons
     WeaponsSalmon(
         id = listOf(22900),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/9/9e/S3_Weapon_Main_Grizzco_Charger_2D_Current.png/128px-S3_Weapon_Main_Grizzco_Charger_2D_Current.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/9/9e/S3_Weapon_Main_Grizzco_Charger_2D_Current.png/64px-S3_Weapon_Main_Grizzco_Charger_2D_Current.png"
     ),
     WeaponsSalmon(
         id = listOf(21900),
-        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/a/a8/S3_Weapon_Main_Grizzco_Roller_2D_Current.png/250px-S3_Weapon_Main_Grizzco_Roller_2D_Current.png"
+        image = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/a/a8/S3_Weapon_Main_Grizzco_Roller_2D_Current.png/64px-S3_Weapon_Main_Grizzco_Roller_2D_Current.png"
     ),
 
 
--- app/src/main/java/com/powerdino/splatoon3_companion/ui/screens/routes/salmonComposables/SalmonRunMapCard.kt
+++ app/src/main/java/com/powerdino/splatoon3_companion/ui/screens/routes/salmonComposables/SalmonRunMapCard.kt
@@ -1,10 +1,7 @@
 package com.powerdino.splatoon3_companion.ui.screens.routes.salmonComposables
 
-import android.util.Log
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,28 +14,19 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextIndent
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.powerdino.splatoon3_companion.R
 import com.powerdino.splatoon3_companion.data.lists.SalmonRunWeapons
-import kotlin.text.Typography.bullet
 
 @Composable
 fun SalmonMapCard(
@@ -90,7 +78,10 @@ fun SalmonMapCard(
                                 if (e == id) {
                                     AsyncImage(
                                         contentScale = ContentScale.Crop,
-                                        model = element.image,
+                                        model = ImageRequest.Builder(LocalContext.current)
+                                            .data(element.image)
+                                            .crossfade(true)
+                                            .build(),
                                         contentDescription = e.toString(),
                                         modifier = Modifier
                                             .padding(bottom = 2.dp)
